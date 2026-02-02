ac2bd1b7df0ba58582fb0f452d77c782fb9ad8c4
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-02-02 15:13:08 +0100

    add polishing and readme

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-
-
-/* 
-Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-
-Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-
-il prezzo del biglietto è definito in base ai km (0.21 € al km)
-va applicato uno sconto del 20% per i minorenni
-va applicato uno sconto del 40% per gli over 65.
-L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca. 
-*/
-
-
-
-
-
-
-
-/* Ask to the user age and route length, add in console */
-
 /* 
 
 tools 
@@ -32,6 +11,7 @@ tools
 
 */
 
+/* ask to the user age and route length, add in console */
 
 const route_length = Number(prompt("Please, add your km route length (number only)"))
 const user_age = Number(prompt("Please, add your age here (number only)"))
